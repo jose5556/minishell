b16b7cda822f12b60935395176e5b37200539dd0
jose5556 <josefilmeneses@gmail.com>
2024-11-27 06:35:04 +0000

    init_signals func and some comments

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 15:58:13 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/27 04:13:43 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/27 06:33:27 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,20 @@ int	main(void)
 	char	*line;
 
 	line = NULL;
+	init_signals();    // ctr + d TODO
 	while (1)
 	{
 		get_prompt();
-		line = read_line(0, line);
-		//line = readline(line); func to use, bit it has so much leaks, that i comented for now
-		if (line == NULL)
-			ft_printf("NULL\n");
-		else
+		//line = read_line(0, line);
+		line = readline(line); //func to use, bit it has so much leaks, that i comented for now
+		if (!line)
+			exit (EXIT_SUCCESS);
+		if (line)
+		{
 			ft_printf("%s\n", line);
-		free(line);
+			free(line);
+			line = NULL;
+		}
 	}
 	return (1);
 }
