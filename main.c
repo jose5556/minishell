d42b6819fec01ec7f0f3871e2558bf56d4326a5d
jose5556 <josefilmeneses@gmail.com>
2024-11-28 00:26:11 +0000

    call the new funcs

--- main.c
+++ main.c
@@ -6,22 +6,30 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 15:58:13 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/27 06:58:58 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/28 00:24:14 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/minishell.h"
 
+int	g_signal;
+
 int	main(void)
 {
-	char	*line;
+	char		*line;
+	t_prompt	*prompt;
 
 	line = NULL;
-	init_signals();    // ctr + \ TODO
+	prompt = NULL;
+	prompt = malloc(sizeof(t_prompt));
+	init(&prompt);
+	if (g_signal == 2)
+	{
+		g_signal = 0;
+	}
 	while (1)
 	{
-		get_prompt();
-		line = readline(line);
+		line = ft_readline(prompt, &line);
 		if (!line)
 			exit (EXIT_SUCCESS);
 		if (line)
