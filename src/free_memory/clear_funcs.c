ef871c57b9ac642749a54461cd46daa40d27ee38
jose5556 <josefilmeneses@gmail.com>
2024-11-28 18:57:42 +0000

    file created to add the funcs to clean memory

---
+++ src/free_memory/clear_funcs.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clear_funcs.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 18:42:59 by joseoliv          #+#    #+#             */
+/*   Updated: 2024/11/28 18:45:39 by joseoliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+void	ft_clear_all(t_prompt *prompt)
+{
+	free(prompt->logname);
+	free(prompt->hostname);
+	free(prompt->pwd);
+	free(prompt->home_directory);
+}
