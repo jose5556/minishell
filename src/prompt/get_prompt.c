b686b63b34b7021a97e9a16f2c70ca957e5a0437
jose5556 <josefilmeneses@gmail.com>
2024-11-28 21:02:44 +0000

    removed todo comment (done)

--- src/prompt/get_prompt.c
+++ src/prompt/get_prompt.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 19:42:41 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/28 00:31:43 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/28 21:00:01 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,8 @@ void	get_prompt(t_prompt *prompt)
 		prompt->logname = get_logname();
 	if (!(prompt->hostname))
 		prompt->hostname = get_hostname();
-	if (!(prompt->home_directory))
-		prompt->home_directory = get_home();
+	if (!(prompt->home_path))
+		prompt->home_path = get_home();
 	prompt->pwd = get_pwd(prompt);
 	print_prompt(prompt);
 }
-
-//TODO
-//FUNCS TO CLEAN ALL OF THE THIS PROMPT VALUES GARBAGE
