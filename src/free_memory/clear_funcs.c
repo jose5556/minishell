59be7af1a8153e3f85d77caf6685da34c4919101
jose5556 <josefilmeneses@gmail.com>
2024-11-28 21:02:12 +0000

    begining of the func to clear all memory allocated

--- src/free_memory/clear_funcs.c
+++ src/free_memory/clear_funcs.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 18:42:59 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/28 18:45:39 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/28 20:58:24 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ void	ft_clear_all(t_prompt *prompt)
 	free(prompt->logname);
 	free(prompt->hostname);
 	free(prompt->pwd);
-	free(prompt->home_directory);
+	free(prompt->home_path);
+	exit(EXIT_SUCCESS);
 }
