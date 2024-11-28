5caafa70eb1c8459f3c4353171172b3121b5eec2
jose5556 <josefilmeneses@gmail.com>
2024-11-28 21:00:46 +0000

    g_var commented and declaration of ft_clear_all func

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 15:58:13 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/28 18:54:37 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/28 20:43:15 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/minishell.h"
 
-int	g_signal;
+//int	g_signal;
 
 int	main(void)
 {
@@ -22,15 +22,11 @@ int	main(void)
 	line = NULL;
 	prompt = ft_calloc(sizeof(t_prompt), 1);
 	init();
-	if (g_signal == 2)
-	{
-		g_signal = 0;
-	}
 	while (1)
 	{
 		line = ft_readline(prompt, &line);
 		if (!line)
-			exit (EXIT_SUCCESS);
+			ft_clear_all(prompt);
 		if (line)
 		{
 			ft_printf("%s\n", line);
@@ -38,6 +34,5 @@ int	main(void)
 			line = NULL;
 		}
 	}
-	ft_clear_all(prompt);
 	return (1);
 }
