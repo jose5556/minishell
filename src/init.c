b63c6e56142df07086d2ecd585d8b52004886309
jose5556 <josefilmeneses@gmail.com>
2024-11-28 18:57:00 +0000

    no need of prompt in init func

--- src/init.c
+++ src/init.c
@@ -6,15 +6,14 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 21:41:14 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/28 00:20:42 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/28 18:54:59 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void	init(t_prompt **prompt)
+void	init()
 {
 	g_signal = 0;
 	init_signals();     // ctr + \ TODO
-	ft_bzero(*prompt, sizeof(t_prompt *));
 }
