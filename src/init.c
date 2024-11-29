dd3d7b9ce1d67ba54df456f6c15b60555bda03dd
jose5556 <josefilmeneses@gmail.com>
2024-11-29 00:21:04 +0000

    reorganized func init_signals

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 21:41:14 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/28 20:45:48 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/29 00:18:43 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,16 @@ void	init()
 {
 	//g_signal = 0;
 	init_signals();     // ctr + \ TODO
+}
+
+void	init_signals()
+{
+	struct sigaction	sa;
+
+	sa.sa_handler = &signal_handler;
+	sa.sa_flags = SA_RESTART;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		exit(1);
+	signal(SIGQUIT, SIG_IGN);
 }
