d5fc75eaae4c72547cdd6ab8a7ffd572134891da
jose5556 <josefilmeneses@gmail.com>
2024-11-29 00:09:50 +0000

    on SIGINT prints the prompt back

--- src/signals/handle_signals.c
+++ src/signals/handle_signals.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 06:32:14 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/28 20:43:28 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/28 23:22:07 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,21 @@ static void	signal_handler(int signum)
 {
 	if (signum == SIGINT)
 	{
-		return ;  //TODO
-	}					// CALL THE PROMPT AGAIN, I HAVE NO IDEA HOW
+		rl_replace_line("", 0);
+		printf("\n");
+		rl_on_new_line();
+		rl_redisplay();
+	}
 }
 
 void	init_signals()
 {
 	struct sigaction	sa;
 
-	signal(SIGQUIT, SIG_IGN);
 	sa.sa_handler = &signal_handler;
-	sa.sa_flags = 0;
+	sa.sa_flags = SA_RESTART;
 	sigemptyset(&sa.sa_mask);
 	if (sigaction(SIGINT, &sa, NULL) == -1)
 		exit(1);
+	signal(SIGQUIT, SIG_IGN);
 }
