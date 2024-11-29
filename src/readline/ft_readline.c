7b9fb96c9eb1492b6ba01c4ed1ae9f5c4d0778bb
jose5556 <josefilmeneses@gmail.com>
2024-11-29 00:16:46 +0000

    updated the argument of the func readline

--- src/readline/ft_readline.c
+++ src/readline/ft_readline.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 22:36:40 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/27 23:52:49 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/29 00:15:06 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 char	*ft_readline(t_prompt *prompt, char **line)
 {
-	get_prompt(prompt);
-	*line = readline(*line);
+	char	*str_prompt;
+
+	str_prompt = NULL;
+	str_prompt = get_prompt(prompt);
+	*line = readline(str_prompt);
 	//parse_line(); TODO ?
 	return (*line);
 }
