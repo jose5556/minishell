b6cdbbdf237f71bb0bc7cd52ca993f17071521c7
jose5556 <josefilmeneses@gmail.com>
2024-11-29 00:08:45 +0000

    func to free some strings that need to be updated in all iterations

--- src/free_memory/clear_funcs.c
+++ src/free_memory/clear_funcs.c
@@ -6,11 +6,21 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 18:42:59 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/28 20:58:24 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/29 00:04:20 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+void	free_necessary(char	**line, t_prompt *prompt)
+{
+	free(*line);
+	free(prompt->final_str);
+	free(prompt->pwd);
+	*line = NULL;
+	prompt->pwd = NULL;
+	prompt->final_str = NULL;
+}
 
 void	ft_clear_all(t_prompt *prompt)
 {
