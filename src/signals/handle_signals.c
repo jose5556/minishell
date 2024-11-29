027093363eda9050edf1ea27125abf663dd8b5d9
jose5556 <josefilmeneses@gmail.com>
2024-11-29 00:21:13 +0000

    reorganized func init_signals

--- src/signals/handle_signals.c
+++ src/signals/handle_signals.c
@@ -6,13 +6,13 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 06:32:14 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/28 23:22:07 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/29 00:18:54 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static void	signal_handler(int signum)
+void	signal_handler(int signum)
 {
 	if (signum == SIGINT)
 	{
@@ -21,16 +21,4 @@ static void	signal_handler(int signum)
 		rl_on_new_line();
 		rl_redisplay();
 	}
-}
-
-void	init_signals()
-{
-	struct sigaction	sa;
-
-	sa.sa_handler = &signal_handler;
-	sa.sa_flags = SA_RESTART;
-	sigemptyset(&sa.sa_mask);
-	if (sigaction(SIGINT, &sa, NULL) == -1)
-		exit(1);
-	signal(SIGQUIT, SIG_IGN);
 }
