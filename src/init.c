f7ea6ac488a44b6faf0e4e2840b301de595f9fe8
jose5556 <josefilmeneses@gmail.com>
2024-12-05 08:32:22 +0000

    init() deleted

--- src/init.c
+++ src/init.c
@@ -6,17 +6,11 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 21:41:14 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/29 00:18:43 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/12/05 08:31:12 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-
-void	init()
-{
-	//g_signal = 0;
-	init_signals();     // ctr + \ TODO
-}
 
 void	init_signals()
 {
