bbf6ceba07887375af692ae6a8c8e74cbc517677
jose5556 <josefilmeneses@gmail.com>
2024-12-05 16:23:05 +0000

    split and func to parse the command

---
+++ src/parse/parse_commands.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_commands.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 20:59:05 by joseoliv          #+#    #+#             */
+/*   Updated: 2024/12/05 16:09:02 by joseoliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+void	parse_line(char **parsed_command, t_bash **bash)
+{
+	char	*command;
+
+	command = NULL;
+	while (*parsed_command)
+	{
+		if (get_command_type(*parsed_command) == COMMAND)
+		{
+			while (get_command_type(*parsed_command) == COMMAND)
+			{
+				command = ft_strjoin_free(command, *parsed_command, 1);
+				parsed_command++;
+			}
+			addback_bash(command, bash);
+			free(command);
+			command = NULL;
+		}
+		else if (get_command_type(*parsed_command) != NOTHING)
+		{
+			command = ft_strjoin_free(command, *parsed_command, 1);
+			command = ft_strjoin_free(command, " ", 1);
+			parsed_command++;
+		}
+	}
+}
